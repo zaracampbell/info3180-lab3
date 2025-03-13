91a6ec93dafb6c44d3c54db3404c40c1a9ad17c5
zaracampbell <campbellzara13@gmail.com>
2025-03-13 02:29:49 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,7 +7,11 @@ class Config(object):
     """Base Config Object"""
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'localhost')
-    MAIL_PORT = os.environ.get('MAIL_PORT', '25')
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'sandbox.smtp.mailtrap.io')
+    MAIL_PORT = os.environ.get('MAIL_PORT', '2525')
+    MAIL_USERNAME = os.environ.get('8d2aa123f58df6')
+    MAIL_PASSWORD = os.environ.get('4926aa576a89e7')
+    
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
