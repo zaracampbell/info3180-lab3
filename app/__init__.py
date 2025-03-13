a9356327469f6807538f94370107052cd09de294
zaracampbell <campbellzara13@gmail.com>
2025-03-13 03:46:32 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_mail import Mail
 from .config import Config
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(Config)
